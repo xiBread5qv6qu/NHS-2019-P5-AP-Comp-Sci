c500d2c8a616fd25a1cde2802ac0dd9dec265259
xiBread5qv6qu <mangongk788585@yeah.net>
2021-03-26 10:45:14 +0800

    Checkpoint 3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -1,28 +1,35 @@
 /**
  * @author Mr. Rasmussen
  */
-
 package fracCalc;
 
 import java.util.*;
 
 public class FracCalc {
-	static int idem2 = 0;
+	static int idem2 = 1;
 	static int inum2 = 0;
 	static int iwhole2 = 0;
-	static int idem1 = 0;
+	static int idem1 = 1;
 	static int inum1 = 0;
 	static int iwhole = 0;
+	static int recip = 1;
+	static int wholeans = 0;
+	static int fractans = 0;
 
 	public static void main(String[] args) {
 		// TODO: Read the input from the user and call produceAnswer with an equation
-		String input;
 		Scanner userInput = new Scanner(System.in);
-		input = userInput.nextLine();
-		produceAnswer(input);
-		userInput.close();
-		System.out.println("whole:" + iwhole2 + " numerator:" + inum2 + " denominator:" + idem2);
-		//System.out.println("whole:" + iwhole + " numerator:" + inum1 + " denominator:" + idem1);
+		String input;
+		boolean running = true;
+		while (running) {
+			input = userInput.nextLine();
+			if (input.equals("Quit")) {	
+				break;
+			}else {
+				produceAnswer(input);
+			}
+			//userInput.close();
+		}
 	}
 
 	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
@@ -38,143 +45,121 @@ public class FracCalc {
 	// e.g. return ==> "1_1/4"
 	public static String produceAnswer(String input) {
 		// TODO: Implement this function to produce the solution to the input
+		idem2 = 1;
+		inum2 = 0;
+		iwhole2 = 0;
+		idem1 = 1;
+		inum1 = 0;
+		iwhole = 0;
+		recip = 1;
+		wholeans = 0;
+		fractans = 0;
 		int a = input.indexOf(" ");
-		String num2 = "0";
-		String dem2 = "1";
-		String whole2 = "0";
-		String num1 = "0";
-		String dem1 = "1";
-		String whole = "0";
 		String answer = "0";
 		char op = input.charAt(a + 1);
 		String imp1 = input.substring(0, a);
 		String imp2 = input.substring(a + 3);
-		//System.out.println("abc");
-		
-		
 		if (imp2.indexOf("/") >= 0 && imp1.indexOf("/") >= 0) {
-			if (imp2.indexOf("/") >= 0) {
-				int slash = imp2.indexOf("/");
-				dem2 = imp2.substring(slash + 1);
-				if (imp2.indexOf("_") >= 0) {
-					int floor = imp2.indexOf("_");
-					num2 = imp2.substring(floor + 1, slash);
-					whole2 = imp2.substring(0, floor);
-				} else {
-					num2 = imp2.substring(0, slash);
-				}
-	
-			} else {
-				whole2 = imp2;
-			}
-			
-			if (imp1.indexOf("/") >= 0) {
-				int slash = imp1.indexOf("/");
-				dem1 = imp1.substring(slash + 1);
-				if (imp1.indexOf("_") >= 0) {
-					int floor = imp1.indexOf("_");
-					num1 = imp1.substring(floor + 1, slash);
-					//System.out.println(num1);
-					whole = imp1.substring(0, floor);
-				} else {
-					num1 = imp1.substring(0, slash);
-				}
-	
-			} else {
-				whole = imp1;
-			}
-		
-		
+			parce2(imp2);
+			parce1(imp1);
+		} else if (imp2.indexOf("/") >= 0) {
+			iwhole = parse(imp1);
+			parce2(imp2);
+		} else if (imp1.indexOf("/") >= 0) {
+			iwhole2 = parse(imp2);
+			parce1(imp1);
+		} else {
+			iwhole = parse(imp1);
+			iwhole2 = parse(imp2);
 		}
-		
-	
-		else if (imp2.indexOf("/") >= 0) {
-			whole = imp1;
-			int slash = imp2.indexOf("/");
-			dem2 = imp2.substring(slash + 1);
-			if (imp2.indexOf("_") >= 0) {
-				int floor = imp2.indexOf("_");
-				num2 = imp2.substring(floor + 1, slash);
-				whole2 = imp2.substring(0, floor);
-			} else if (imp2.indexOf("_") <= 0) {
-				num2 = imp2.substring(0, slash);
-			}
-	
-			else {
-			whole2 = imp2;
-			}
+		if (iwhole < 0) {
+			inum1 *= -1;
 		}
-		
-		else if (imp1.indexOf("/") >= 0) {
-			//System.out.println("abc");
-			whole2 = imp2;
-			//System.out.println(whole2);
-			int slash = imp1.indexOf("/");
-			dem1 = imp1.substring(slash + 1);
-			if (imp1.indexOf("_") >= 0) {
-				int floor = imp1.indexOf("_");
-				num1 = imp1.substring(floor + 1, slash);
-				whole = imp1.substring(0, floor);
-			} else if (imp1.indexOf("_") <= 0) {
-				num1 = imp1.substring(0, slash);
-				//System.out.println("abc");
-			}
-	
-			else {
-			whole = imp1;
-			}
+		inum1 = (iwhole * idem1) + inum1;
+		iwhole = 0;
+		if (iwhole2 < 0) {
+			inum2 *= -1;
 		}
-		
-		else {
-			whole = imp1;
-			whole2 = imp2;
-		}
-			
-		
-		//System.out.println(iwhole2);
-		inum2 = Integer.parseInt(num2);
-		//System.out.println(iwhole2);
-		idem2 = Integer.parseInt(dem2);
-		//System.out.println(iwhole2);
-		iwhole2 = Integer.parseInt(whole2);
-		//System.out.println(iwhole2);
-		inum1 = Integer.parseInt(num1);
-		//System.out.println(iwhole2);
-		idem1 = Integer.parseInt(dem1);
-		//System.out.println(iwhole2);
-		iwhole = Integer.parseInt(whole);
-		//System.out.println(inum1);
-		if (imp1.indexOf("_") >= 0) {
-			//inum1 = iwhole * (idem1 + inum1);
-			//iwhole = 0;
-		}
-		if (imp2.indexOf("_") >= 0) {
-			//inum2 = iwhole2 * (idem2 + inum2);
-			//iwhole2 = 0;
-		}
-		//idem1 *= idem2;
-		//inum1 *= inum2;
-		//System.out.println(iwhole2);
-		//System.out.println(num2);
+		inum2 = (iwhole2 * idem2) + inum2;
+		iwhole2 = 0;
 		if (op == '*') {
 			inum1 *= inum2;
 			idem1 *= idem2;
-			int wholeans = inum1 / idem1;
-			int fractans = inum1 % idem1;
-			answer = (wholeans + "_" + fractans + "/" + idem1);
+			wholeans = inum1 / idem1;
+			fractans = inum1 % idem1;
+		} else if (op == '/') {
+			recip = idem2;
+			idem2 = inum2;
+			inum1 *= recip;
+			idem1 *= idem2;
+			wholeans = inum1 / idem1;
+			fractans = inum1 % idem1;
+		} else if (op == '+') {
+			inum1 *= idem2;
+			inum2 *= idem1;
+			idem1 *= idem2;
+			idem2 = idem1;
+			inum1 += inum2;
+			wholeans = inum1 / idem1;
+			fractans = Math.abs(inum1 % idem1);
+		} else {
+			inum1 *= idem2;
+			inum2 *= idem1;
+			idem1 *= idem2;
+			idem2 = idem1;
+			inum1 -= inum2;
+			wholeans = inum1 / idem1;
+			fractans = inum1 % idem1;
 		}
-		String h = ("whole:" + iwhole2 + " numerator:" + inum2 + " denominator:" + idem2);
-		//System.out.println(answer);
-		//System.out.println(idem1);
-		//System.out.println(iwhole);
-		//System.out.println(inum1);
-		//System.out.println(idem2);
-		//System.out.println(iwhole2);
-		return h;
-
+		int gcf = gdc(fractans, idem1);
+		fractans /= gcf;
+		idem1 /= gcf;
+		if (fractans != 0 && wholeans != 0) {
+			answer = (wholeans + "_" + Math.abs(fractans) + "/" + Math.abs(idem1));
+		} else if (fractans != 0 && wholeans == 0) {
+			answer = (fractans + "/" + Math.abs(idem1));
+		} else if (fractans == 0 && wholeans != 0) {
+			answer = (wholeans + "");
+		}
+		System.out.println(answer);
+		return answer;
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
 	// need
+	public static int parse(String a) {
+		int i = Integer.parseInt(a);
+		return i;
+	}
 
+	public static int gdc(int num1, int num2) {
+		if (num2 == 0) {
+			return num1;
+		}
+		return gdc(num2, num1 % num2);
+	}
+
+	public static void parce1(String imp) {
+		int slash = imp.indexOf("/");
+		idem1 = parse(imp.substring(slash + 1));
+		if (imp.indexOf("_") >= 0) {
+			int floor = imp.indexOf("_");
+			inum1 = parse(imp.substring(floor + 1, slash));
+			iwhole = parse(imp.substring(0, floor));
+		} else if (imp.indexOf("_") <= 0) {
+			inum1 = parse(imp.substring(0, slash));
+		}
+	}
+
+	public static void parce2(String imp) {
+		int slash = imp.indexOf("/");
+		idem2 = parse(imp.substring(slash + 1));
+		if (imp.indexOf("_") >= 0) {
+			int floor = imp.indexOf("_");
+			inum2 = parse(imp.substring(floor + 1, slash));
+			iwhole2 = parse(imp.substring(0, floor));
+		} else if (imp.indexOf("_") <= 0) {
+			inum2 = parse(imp.substring(0, slash));
+		}
+	}
 }
